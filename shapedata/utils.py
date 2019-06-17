c641a2c697f4dc6448dfd6f8ce7998bfa3c72e61
Paul Kruse <paul@pbnl.de>
2019-06-17 21:43:50 +0200

    Updated to delira 0.4

--- shapedata/utils.py
+++ shapedata/utils.py
@@ -47,7 +47,7 @@ def is_landmark_file(filename):
     return any(filename.endswith(extension) for extension in LMK_EXTENSIONS)
 
 
-def make_dataset(dir):
+def make_dataset(dir, look_for_lmk_extensions= LMK_EXTENSIONS):
     """
     Helper Function to make a dataset containing all images in a certain
     directory
@@ -70,7 +70,7 @@ def make_dataset(dir):
             if is_image_file(fname) and any(
                     [os.path.isfile(os.path.join(root,
                                                  fname.rsplit(".", 1)[0] + ext)
-                                    ) for ext in LMK_EXTENSIONS]):
+                                    ) for ext in look_for_lmk_extensions]):
                 path = os.path.join(root, fname)
                 images.append(path)
 
